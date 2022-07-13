c3e2a084afb6b5db9ec754db87531a7c96283c36
Chung YanHo <Chung.YanHo@theaccessgroup.com>
2022-07-13 10:53:14 +0100

    Adding-An-App-Bar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
-import { Typography } from "@mui/material";
+
 import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import { Product } from "../models/product";
+import Header from "./Header";
 
 
 function App() {
@@ -27,7 +28,8 @@ function App() {
 
   return (
       <>
-          <Typography variant ='h1'>Justin's Re-Store</Typography>
+         <Header />
+        
           <Catalog products={products} addProduct={addProduct}/>
          
     </>
